6f8c56c86762ca8600b0afeded0dcdc417d8168a
Loganathan <loganathanengrr@gmail.com>
2022-07-12 15:55:58 +0530

    python file updated

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
-from fastapi import FastAPI
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = pathlib.Path(__file__).parent
+ROOT_PROJECT_DIR = BASE_DIR.parent
+TEMPLATE_DIR = ROOT_PROJECT_DIR / "html"
 
 app = FastAPI()
+templates = Jinja2Templates(directory=str(TEMPLATE_DIR))
+
 
 @app.get('/')
-def read_index():
-    return {"Hello":"World"}
+def read_index(request: Request):
+    context = {
+        "request":request,
+        "title":"Fast API",
+    }
+    return templates.TemplateResponse("index.nginx-debian.html", context)
 
 @app.get("/abc")
 def abc():
